0b6681d4ddefb48101f8022d51012061dfaa3d2a
AngelineTung <angelinetung@hotmail.com>
2025-05-15 16:39:01 +0800

    creating application using terraform

---
+++ helm/variables.tf
@@ -0,0 +1,14 @@
+variables "admin" {
+	type = string 
+	description = "Username Value"
+}
+
+variable "password" {
+	type = string
+	description = "Password Value"
+}
+
+variable "server_addr" {
+  type = string
+  description = "server_addr Value"
+}
